6c8e8d498cf63e993b5419685ba0f90a9eea9a7d
Fábio André Vilar Lourenço <1150434@isep.ipp.pt>
2017-01-24 13:39:42 +0000

    Select Game Mode activity

---
+++ app/src/main/java/com/trimteam/tictactoe/SelectGameMode.java
@@ -0,0 +1,13 @@
+package com.trimteam.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectGameMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_game_mode);
+    }
+}
