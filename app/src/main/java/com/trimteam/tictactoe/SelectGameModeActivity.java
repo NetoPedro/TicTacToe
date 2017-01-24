be6ebe8c7f28e6cf953ccfc966b168c24c90e480
Fábio André Vilar Lourenço <1150434@isep.ipp.pt>
2017-01-24 13:49:14 +0000

    Select Difficulty activity

--- app/src/main/java/com/trimteam/tictactoe/SelectGameModeActivity.java
+++ app/src/main/java/com/trimteam/tictactoe/SelectGameModeActivity.java
@@ -3,7 +3,7 @@ package com.trimteam.tictactoe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SelectGameMode extends AppCompatActivity {
+public class SelectGameModeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
